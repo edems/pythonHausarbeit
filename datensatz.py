71706728299d411ce4b245ecf46ca26ca2fbc771
adam <adam.horn@gmx.de>
2023-05-28 14:57:05 +0200

    lösung aufgabe 1

--- datensatz.py
+++ datensatz.py
@@ -49,12 +49,19 @@
 #     # Verbindung schließen (automatisch durch das 'with'-Statement)
 #     session.close()
 
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
 matplotlib.use('Qt5Agg')
+from bokeh.plotting import figure, output_file, show
+from bokeh.io import output_notebook
+
+output_notebook()
+
 class Data:
     def __init__(self, x, y):
         self.x = x
@@ -67,31 +74,42 @@ class TestData(Data):
     pass
 
 class IdealFunction(Data):
-    # Implementierung der Vorhersagefunktion für die ideale Funktion
-    def predict(self, x):
+    pass # Implementierung der Vorhersagefunktion für die ideale Funktion
 
-        return np.interp(x, self.x, self.y)
 
 class QuadraticFitting:
     def __init__(self, train_data, ideal_functions):
         self.train_data = train_data
         self.ideal_functions = ideal_functions
 
-    def fit(self):
+    def fit2(self, test_data):
         best_fits = []
+        test_deviations = []
         for train_data in self.train_data:
             best_fit = None
             min_sum_squared_diff = float('inf')
+            train_x = train_data.x
+            train_y = train_data.y
+            print("neuer satz")
+            j = 0
             for ideal_function in self.ideal_functions:
-                ideal_y = ideal_function.predict(train_data.x)
-                if (train_data.y is None) or (ideal_y is None):
+                j += 1
+                ideal_x = ideal_function.x
+                ideal_y = ideal_function.y
+                if (train_y is None) or (ideal_y is None):
                     break
                 else:
-                    sum_squared_diff = np.sum(np.square(train_data.y - ideal_y))
+                    A = np.vstack([ideal_x, np.ones(len(ideal_x))]).T
+                    m, c = np.linalg.lstsq(A, ideal_y, rcond=None)[0]
+                    ideal_y_fit = m * train_x + c
+                    sum_squared_diff = np.sum(np.square(train_y - ideal_y_fit))
+
                 if sum_squared_diff < min_sum_squared_diff:
                     min_sum_squared_diff = sum_squared_diff
                     best_fit = ideal_function
+                    print(j)
             best_fits.append(best_fit)
+
         return best_fits
 
 # Funktion zum Laden der Daten aus einer CSV-Datei mit pandas
@@ -106,32 +124,20 @@ train_x, train_y = load_data_from_csv('train.csv')
 test_x, test_y = load_data_from_csv('test.csv')
 ideal_x, ideal_y = load_data_from_csv('ideal.csv')
 
-# Beispiel-Daten erstellen
+# Daten erstellen
 train_data = [TrainingData(train_x, train_y[:, i]) for i in range(train_y.shape[1])]
 test_data = [TestData(test_x, test_y[:, i]) for i in range(test_y.shape[1])]
-ideal_functions = [IdealFunction(ideal_x, ideal_y[:, i]) for i in range(ideal_y.shape[1])]
+ideal_functions2 = [IdealFunction(ideal_x, ideal_y[:, i]) for i in range(ideal_y.shape[1])]
 
-# QuadraticFitting durchführen
-quadratic_fitting = QuadraticFitting(train_data, ideal_functions)
-best_fits = quadratic_fitting.fit()
-
-# Ausgabe der besten Anpassungen
-for i, fit in enumerate(best_fits):
-    print(f"Best fit {i+1}: {fit}")
+quadratic_fitting2 = QuadraticFitting(train_data, ideal_functions2)
+best_fits2 = quadratic_fitting2.fit2(ideal_functions2)
 
 # Visualisierung der Daten und Anpassungen
 for i, train_data in enumerate(train_data):
-    # print("Das ist X" + train_data.x + " Das ist Y" + train_data.y + "Das ist best Fits" )
-    # print("-------------------------------------------------------------------------------------------------------------------------------------------------")
-    # print(" Das ist X")
-    # print(train_data.x)
-    # print(" Das ist Y")
-    # print(train_data.y)
-    plt.scatter(train_data.x, train_data.y, label='Train Data')
-    plt.plot(train_data.x, best_fits[i].predict(train_data.x), label='Best Fit')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title(f'Best Fit {i+1}')
-    plt.legend()
+    p = figure(title=f'Best Fit {i+1}', x_axis_label='x', y_axis_label='y')
+    p.scatter(train_data.x, train_data.y, legend_label='Train Data', color='blue')
+    p.line(train_data.x, best_fits2[i].predict(train_data.x), legend_label='Best Fit2', color='red')
+    output_file(f'best_fit_{i+1}.html')
+    show(p)
 
-plt.show()
+
