e566bfceb2b6e644c2cb5fdf367089a0d9dca7f0
adam <adam.horn@gmx.de>
2023-06-08 18:08:55 +0200

    unit test hinzugefügt

--- utestpythontask.py
+++ utestpythontask.py
@@ -1,44 +1,91 @@
 import unittest
-from dataprocessing import TrainData, TestData, IdealData, QuadraticFitting
-from sqlhelper import SQLHelperKlasse
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-
-
-        try:
-            self.train_file = 'train.csv'
-            self.test_file = 'test.csv'
-            self.ideal_file = 'ideal.csv'
-            self.helper = SQLHelperKlasse()
-            # Daten aus CSV-Dateien laden
-            train = TrainData(self.train_file)
-            test = TestData(self.test_file)
-            ideal = IdealData(self.ideal_file)
+from dataprocessing import QuadraticFitting , TrainData, BestFits
+import unittest
+import pandas as pd
+import numpy as np
 
 
+class QuadraticFittingTestCase(unittest.TestCase):
+    """
+    Unittests für die QuadraticFitting-Klasse.
+    """
 
-        except FileNotFoundError:
-            print("Eine oder mehrere der CSV-Dateien wurden nicht gefunden.")
-        except PermissionError:
-            print("Keine Berechtigung zum Lesen oder Schreiben der Dateien.")
-        except ValueError:
-            print("Ein Wertefehler ist aufgetreten.")
-        except Exception as e:
-            print("Fehler beim Ausführen des Tests:", str(e))
-        # add assertion here
-# class TestQuadraticFitting:
-#     def __init__(self):
-#
-#
-#     def run(self):
-#
+    def setUp(self):
+        # Initialisierung der Testdaten und -objekte
+        self.train_data = pd.read_csv('train.csv')
+        self.ideal_functions = pd.read_csv('ideal.csv')
+        self.best_fit = pd.read_csv('test.csv')
 
-#
-# # Testklasse ausführen
-# test = TestQuadraticFitting()
-# test.run()
+    def tearDown(self):
+        # Aufräumen nach jedem Testfall
+        pass
+
+    def test_search_y_at_x(self):
+        # Testet die search_y_at_x-Methode der QuadraticFitting-Klasse.
+
+        # Vorbereitung der Testdaten
+        ideal_data = pd.DataFrame({'x': self.ideal_functions['x'], 'y': self.ideal_functions['y1']})
+        x = -19.1
+        expected_y = -0.2478342
+
+        # Aufruf der zu testenden Methode
+        y = QuadraticFitting.search_y_at_x(x, ideal_data)
+
+        # Überprüfung des erwarteten Ergebnisses
+        self.assertEqual(y, expected_y)
+
+    def test_create_traindata_structure(self):
+        # Testet die create_traindata_structure-Methode der QuadraticFitting-Klasse.
+
+        # Vorbereitung der Testdaten
+        traindata = self.train_data
+        test_trainData = TrainData('train.csv')
+
+        # Überprüfung, ob die erzeugte Datenstruktur korrekt ist
+        self.assertTrue(test_trainData.data_frame.equals(traindata))
+
+    def test_best_fits(self):
+        # Testet die best_fits-Methode der QuadraticFitting-Klasse.
+
+        # Vorbereitung der Testdaten und -objekte
+        test_trainData = TrainData('train.csv')
+        test_IdealData = TrainData('ideal.csv')
+        test_best_fits = BestFits(test_trainData.data_frame)
+
+        for train_data in test_trainData:
+            fits = []
+
+            # Extrahieren der x- und y-Werte aus dem Trainingsdatensatz
+            train_x = train_data.x
+            train_y = train_data.y
+
+            for ideal_function in test_IdealData:
+                ideal_x = ideal_function.x
+                ideal_y = ideal_function.y
+
+                try:
+                    # Berechnung der besten Anpassung mit Hilfe von Numpy
+                    m, c = np.linalg.lstsq(np.vstack([ideal_x, np.ones(len(ideal_x))]).T, ideal_y, rcond=None)[0]
+                    sum_squared_diff = np.sum(np.square(train_y - (m * train_x + c)))
+                    fits.append((ideal_function, sum_squared_diff))
+                except np.linalg.LinAlgError as np_error:
+                    # Fehler beim Lösen des linearen Gleichungssystems mit numpy
+                    print("Fehler beim Lösen des linearen Gleichungssystems:", str(np_error))
+
+            # Finden der besten Anpassung
+            found_fit = min(fits, key=lambda fit: fit[1])[0] if fits else None
+            test_best_fits.add_data_to_bestfit(found_fit)
+
+        # Füllen der besten Anpassung in ein DataFrame
+        test_best_fits.fill_bestfit_to_df()
+
+        # Ausführen der zu testenden Methode
+        best_fit = QuadraticFitting.best_fits(test_trainData, test_IdealData)
+
+        # Überprüfung, ob die erzeugte beste Anpassung korrekt ist
+        self.assertTrue(best_fit.data_frame_bestfit.equals(test_best_fits.data_frame_bestfit))
 
 
 if __name__ == '__main__':
+    # Führt die Unit-Tests aus
     unittest.main()
