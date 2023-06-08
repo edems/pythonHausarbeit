1a9f95fc225287f00ff234d2384f45af8d1d30e3
adam <adam.horn@gmx.de>
2023-06-08 18:10:42 +0200

    unit test hinzugefügt

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ if __name__ == '__main__':
     # Initialisierung des IdealData-Objekts mit 'ideal.csv'
     ideal = IdealData('ideal.csv')
     # Initialisierung des SQLHelperKlasse-Objekts
-    # sql_helper = SQLHelperKlasse()
+    sql_helper = SQLHelperKlasse()
 
     # Löschen aller Tabellen in der Datenbank
-    # sql_helper.clear_table()
+    sql_helper.clear_table()
     # Schreiben des Trainingsdatensatzes in die Datenbank
-    # sql_helper.df_into_sql(train.data_frame, "training", "Training Data")
+    sql_helper.df_into_sql(train.data_frame, "training", "Training Data")
     # Schreiben des Idealdatensatzes in die Datenbank
-    # sql_helper.df_into_sql(ideal.data_frame, "ideal", "Ideal Data")
+    sql_helper.df_into_sql(ideal.data_frame, "ideal", "Ideal Data")
     # Durchführung der QuadraticFitting-Berechnung
     fits_result = QuadraticFitting.best_fits(train, ideal)
     # Anzeige des Ergebnisses von Aufgabe 1
-    # QuadraticFitting.show_task_one_result(fits_result)
+    QuadraticFitting.show_task_one_result(fits_result)
 
     # Initialisierung des TestData-Objekts mit 'test.csv'
     test = TestData('test.csv')
@@ -27,9 +27,9 @@ if __name__ == '__main__':
     # # Anzeige des Ergebnisses von Aufgabe 2
     QuadraticFitting.show_task_two_result(bestma)
     # # Schreiben des Match-Ergebnisses in die Datenbank mit passender idealer Funktion und die Abweichung
-    # sql_helper.match_tosql(bestma.data_frame_passendematch, "matches", "Result")
-    # # Ausgabe aller Tabelleninhalte aus der Datenbank mit Hilfe von print()
-    # sql_helper.write_all_table()
+    sql_helper.match_tosql(bestma.data_frame_passendematch, "matches", "Result")
+    # # Ausgabe aller Tabelleninhalte aus der Datenbank mithilfe von print()
+    sql_helper.write_all_table()
 
 
 
